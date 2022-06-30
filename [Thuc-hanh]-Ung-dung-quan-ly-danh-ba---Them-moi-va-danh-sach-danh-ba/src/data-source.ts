eac0c7d44d5e08fd3a9e91780a9e87ecdcbee7ab
superman <duonga2qp@gmail.com>
2022-06-30 15:32:14 +0700

    nop bai thuc hanh 2

---
+++ [Thuc-hanh]-Ung-dung-quan-ly-danh-ba---Them-moi-va-danh-sach-danh-ba/src/data-source.ts
@@ -0,0 +1,30 @@
+
+import "reflect-metadata"
+
+import { DataSource } from "typeorm"
+
+
+
+export const AppDataSource = new DataSource({
+
+    type: "mysql",
+
+    host: "localhost",
+
+    port: 3305,
+
+    username: "root",
+
+    password: "password",
+
+    database: "company",
+
+    synchronize: true,
+
+    logging: false,
+
+    entities: ["dist/src/entity/*.js"],
+
+    migrations: ["dist/src/migrations/*.js"],
+
+})
